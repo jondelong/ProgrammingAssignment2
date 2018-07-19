f8914151b7e77261693cca5da59df2f7b5e7558a
jondelong <40028325+jondelong@users.noreply.github.com>
2018-07-19 08:03:48 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inv < NULL
+  set <-function(y){
+    x<<-y
+    inv <<- NULL}
+  get <- function()  x
+  setInverse <- function(inverse) inv <<-inverse
+  getInverse <- function() invlist (set = set,
+                                    get = get,
+                                    setInverse = setInverse,
+                                    getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## Inverse of matrix from makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)){
+    message('getting cahced date")
+return(inc)
+}
+mat <- xSget()
+inv <-solve(mat, ...)
+x$setInverse(inv)
+inv
+}
 }
